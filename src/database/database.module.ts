4fe119174ab14905344d40b4acda7498a518da24
arovi <arrooficr7@gmail.com>
2024-11-06 22:55:42 +0700

    THIRD commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,20 +4,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        url: process.env.DATABASE_URL,
-        host: configService.getOrThrow('MYSQL_HOST'),
-        port: configService.get('MYSQL_PORT'),
-        database: configService.getOrThrow('MYSQL_DATABASE'),
-        username: configService.getOrThrow('MYSQL_USERNAME'),
-        password: configService.getOrThrow('MYSQL_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: process.env.MYSQL_URL, // Gunakan variabel ini
+      autoLoadEntities: true,
+      synchronize: true, // Gunakan false di production
     }),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'mysql',
+    //     url: process.env.DATABASE_URL,
+    //     host: configService.getOrThrow('MYSQL_HOST'),
+    //     port: configService.get('MYSQL_PORT'),
+    //     database: configService.getOrThrow('MYSQL_DATABASE'),
+    //     username: configService.getOrThrow('MYSQL_USERNAME'),
+    //     password: configService.getOrThrow('MYSQL_PASSWORD'),
+    //     autoLoadEntities: true,
+    //     synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
   ],
 })
 export class DatabaseModule {}
