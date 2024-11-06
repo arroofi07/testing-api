a83bbc1be0aff0cc3c148848ffc28772b4ba8430
arovi <arrooficr7@gmail.com>
2024-11-06 23:48:57 +0700

    four commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,28 +2,39 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRootAsync({
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'mysql',
+//         url: process.env.DATABASE_URL,
+//         host: configService.getOrThrow('MYSQL_HOST'),
+//         port: configService.get('MYSQL_PORT'),
+//         database: configService.getOrThrow('MYSQL_DATABASE'),
+//         username: configService.getOrThrow('MYSQL_USERNAME'),
+//         password: configService.getOrThrow('MYSQL_PASSWORD'),
+//         autoLoadEntities: true,
+//         synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+// })
+// export class DatabaseModule {}
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(), // Memuat file .env
     TypeOrmModule.forRoot({
       type: 'mysql',
-      url: process.env.MYSQL_URL, // Gunakan variabel ini
-      autoLoadEntities: true,
-      synchronize: true, // Gunakan false di production
+      url: process.env.MYSQL_URL, // Menggunakan MYSQL_URL dari .env
+      autoLoadEntities: true, // Menyesuaikan entitas otomatis
+      synchronize: true, // Jangan gunakan di production (akan memodifikasi struktur DB)
     }),
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'mysql',
-    //     url: process.env.DATABASE_URL,
-    //     host: configService.getOrThrow('MYSQL_HOST'),
-    //     port: configService.get('MYSQL_PORT'),
-    //     database: configService.getOrThrow('MYSQL_DATABASE'),
-    //     username: configService.getOrThrow('MYSQL_USERNAME'),
-    //     password: configService.getOrThrow('MYSQL_PASSWORD'),
-    //     autoLoadEntities: true,
-    //     synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
-    //   }),
-    //   inject: [ConfigService],
-    // }),
   ],
 })
 export class DatabaseModule {}
